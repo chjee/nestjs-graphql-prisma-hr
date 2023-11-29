7d398d180331077a693671f4bc18e75a175e4018
Changhoon-Jee <chjee71@gmail.com>
2023-11-29 22:51:13 +0900

    add jest functions to region.

--- src/regions/regions.resolver.spec.ts
+++ src/regions/regions.resolver.spec.ts
@@ -1,19 +1,81 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RegionsResolver } from './regions.resolver';
 import { RegionsService } from './regions.service';
+import { Region } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateRegionInput } from './dto/create-region.input';
+import { UpdateRegionInput } from './dto/update-region.input';
+import { CountriesService } from '../countries/countries.service';
 
 describe('RegionsResolver', () => {
   let resolver: RegionsResolver;
+  let service: RegionsService;
+
+  const region: Region = {
+    id: 5,
+    name: 'Antarctica',
+  };
+
+  const regions: Region[] = [
+    {
+      id: 5,
+      name: 'Antarctica',
+    },
+  ];
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RegionsResolver, RegionsService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        PrismaService,
+        RegionsResolver,
+        RegionsService,
+        CountriesService,
+      ],
     }).compile();
 
-    resolver = module.get<RegionsResolver>(RegionsResolver);
+    resolver = moduleRef.get<RegionsResolver>(RegionsResolver);
+    service = moduleRef.get<RegionsService>(RegionsService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  describe('create', () => {
+    it('should create a region', async () => {
+      const createRegionInput: CreateRegionInput = {
+        id: 5,
+        name: 'Antarctica',
+      };
+      jest.spyOn(service, 'create').mockImplementation(async () => region);
+      expect(await resolver.createRegion(createRegionInput)).toBe(region);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of regions', async () => {
+      jest.spyOn(service, 'findAll').mockImplementation(async () => regions);
+      expect(await resolver.findAll(0, 10)).toBe(regions);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a region by id', async () => {
+      jest.spyOn(service, 'findOne').mockImplementation(async () => region);
+      expect(await resolver.findOne(5)).toBe(region);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a region by id', async () => {
+      const updateRegionInput: UpdateRegionInput = {
+        name: 'Antarctic Continent',
+      };
+      jest.spyOn(service, 'update').mockImplementation(async () => region);
+      expect(await resolver.updateRegion(5, updateRegionInput)).toBe(region);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a region by id', async () => {
+      jest.spyOn(service, 'remove').mockImplementation(async () => region);
+      expect(await resolver.removeRegion(5)).toBe(region);
+    });
   });
 });
--- src/regions/regions.service.spec.ts
+++ src/regions/regions.service.spec.ts
@@ -1,18 +1,75 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RegionsService } from './regions.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { Region } from '@prisma/client';
+import { CreateRegionInput } from './dto/create-region.input';
+import { UpdateRegionInput } from './dto/update-region.input';
 
 describe('RegionsService', () => {
   let service: RegionsService;
 
+  const region: Region = {
+    id: 5,
+    name: 'Antarctica',
+  };
+
+  const regions: Region[] = [
+    {
+      id: 5,
+      name: 'Antarctica',
+    },
+  ];
+
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [RegionsService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService, RegionsService],
     }).compile();
 
-    service = module.get<RegionsService>(RegionsService);
+    service = moduleRef.get<RegionsService>(RegionsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create a region', async () => {
+      const createRegionInput: CreateRegionInput = {
+        id: 5,
+        name: 'Antarctica',
+      };
+      jest.spyOn(service, 'create').mockImplementation(async () => region);
+      expect(await service.create(createRegionInput)).toBe(region);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of regions', async () => {
+      jest.spyOn(service, 'findAll').mockImplementation(async () => regions);
+      expect(await service.findAll({ skip: 0, take: 3 })).toBe(regions);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a region by id', async () => {
+      const result: Region = region;
+      jest.spyOn(service, 'findOne').mockImplementation(async () => region);
+      expect(await service.findOne({ id: 5 })).toBe(result);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a region by id', async () => {
+      const updateRegionInput: UpdateRegionInput = {
+        name: 'Antarctic Continent',
+      };
+      jest.spyOn(service, 'update').mockImplementation(async () => region);
+      expect(
+        await service.update({ where: { id: 5 }, data: updateRegionInput }),
+      ).toBe(region);
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a region by id', async () => {
+      jest.spyOn(service, 'remove').mockImplementation(async () => region);
+      expect(await service.remove({ id: 5 })).toBe(region);
+    });
   });
 });
