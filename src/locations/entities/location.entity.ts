86a68c3f68fdbf0962462a880ee63cb0a9df578d
Changhoon-Jee <chjee71@gmail.com>
2023-11-30 21:24:47 +0900

    remove validators.

--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,40 +1,24 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { IsNotEmpty, IsNumber, IsString, Length, Min } from 'class-validator';
 import { Country } from './../../countries/entities/country.entity';
 
 @ObjectType({ description: 'Location object' })
 export class Location {
   @Field(() => Int, { description: 'Location ID' })
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(1)
   id: number;
 
   @Field(() => String, { nullable: true, description: 'Street address' })
-  @IsString()
-  @Length(3, 40)
   streetAddress?: string;
 
   @Field(() => String, { nullable: true, description: 'Postal code' })
-  @IsString()
-  @Length(3, 12)
   postalCode?: string;
 
   @Field(() => String, { description: 'City' })
-  @IsNotEmpty()
-  @IsString()
-  @Length(3, 30)
   city: string;
 
   @Field(() => String, { nullable: true, description: 'State province' })
-  @IsString()
-  @Length(3, 25)
   stateProvince?: string;
 
   @Field(() => String, { description: 'Country ID' })
-  @IsNotEmpty()
-  @IsString()
-  @Length(2, 2)
   countryId: string;
 
   @Field(() => Country, { nullable: true, description: 'Country object' })
--- src/regions/entities/region.entity.ts
+++ src/regions/entities/region.entity.ts
@@ -1,18 +1,12 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { IsNotEmpty, IsNumber, Length, Min } from 'class-validator';
 import { Country } from './../../countries/entities/country.entity';
 
 @ObjectType({ description: 'Region object' })
 export class Region {
   @Field(() => Int, { description: 'Region ID)' })
-  @IsNotEmpty()
-  @IsNumber()
-  @Min(1)
   id: number;
 
   @Field(() => String, { description: 'Region name' })
-  @IsNotEmpty()
-  @Length(3, 25)
   name: string;
 
   @Field(() => [Country], { nullable: true, description: 'Country name' })
