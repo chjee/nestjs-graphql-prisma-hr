f644392cdfb06e2c6a61ecfb82e41330ffb258dd
Changhoon-Jee <chjee71@gmail.com>
2023-11-30 16:39:59 +0900

    add jest func. to locations.

--- src/locations/locations.resolver.spec.ts
+++ src/locations/locations.resolver.spec.ts
@@ -1,19 +1,129 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
 import { LocationsResolver } from './locations.resolver';
 import { LocationsService } from './locations.service';
+import { CountriesService } from '../countries/countries.service';
+// import { Location } from './entities/location.entity';
+import { Location } from '@prisma/client';
+import { Country } from '../countries/entities/country.entity';
+import { CreateLocationInput } from './dto/create-location.input';
+import { UpdateLocationInput } from './dto/update-location.input';
 
 describe('LocationsResolver', () => {
-  let resolver: LocationsResolver;
+  let locationsResolver: LocationsResolver;
+  let locationsService: LocationsService;
+  let countriesService: CountriesService;
+
+  const location: Location = {
+    id: 1100,
+    streetAddress: '93091 Calle della Testa',
+    postalCode: '10934',
+    city: 'Venice',
+    stateProvince: 'Venice',
+    countryId: 'IT',
+  };
+
+  const locations: Location[] = [
+    {
+      id: 1100,
+      streetAddress: '93091 Calle della Testa',
+      postalCode: '10934',
+      city: 'Venice',
+      stateProvince: 'Venice',
+      countryId: 'IT',
+    },
+  ];
+
+  const country: Country = {
+    id: 'KR',
+    name: 'Republic of Korea',
+    regionId: 3,
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationsResolver, LocationsService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        PrismaService,
+        LocationsResolver,
+        LocationsService,
+        CountriesService,
+      ],
     }).compile();
 
-    resolver = module.get<LocationsResolver>(LocationsResolver);
+    locationsResolver = moduleRef.get<LocationsResolver>(LocationsResolver);
+    locationsService = moduleRef.get<LocationsService>(LocationsService);
+    countriesService = moduleRef.get<CountriesService>(CountriesService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  describe('create', () => {
+    it('should create a location', async () => {
+      const createLocationInput: CreateLocationInput = {
+        streetAddress: '93091 Calle della Testa',
+        postalCode: '10934',
+        city: 'Venice',
+        stateProvince: 'Venice',
+        countryId: 'IT',
+      };
+      jest
+        .spyOn(locationsService, 'create')
+        .mockImplementation(async () => location);
+      expect(await locationsResolver.createLocation(createLocationInput)).toBe(
+        location,
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of locations', async () => {
+      jest
+        .spyOn(locationsService, 'findAll')
+        .mockImplementation(async () => locations);
+      expect(await locationsResolver.findAll(0, 10)).toBe(locations);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a location by id', async () => {
+      jest
+        .spyOn(locationsService, 'findOne')
+        .mockImplementation(async () => location);
+      expect(await locationsResolver.findOne(1100)).toBe(location);
+    });
+  });
+
+  describe('country', () => {
+    it('should return a country by location id', async () => {
+      jest
+        .spyOn(countriesService, 'findOne')
+        .mockImplementation(async () => country);
+      expect(await locationsResolver.country(location)).toBe(country);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a location by id', async () => {
+      const updateLocationInput: UpdateLocationInput = {
+        streetAddress: '93091 Calle della Testa',
+        postalCode: '10934',
+        city: 'Venice',
+        stateProvince: 'Venice',
+        countryId: 'IT',
+      };
+      jest
+        .spyOn(locationsService, 'update')
+        .mockImplementation(async () => location);
+      expect(
+        await locationsResolver.updateLocation(1100, updateLocationInput),
+      ).toBe(location);
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a location by id', async () => {
+      jest
+        .spyOn(locationsService, 'remove')
+        .mockImplementation(async () => location);
+      expect(await locationsResolver.removeLocation(1100)).toBe(location);
+    });
   });
 });
--- src/locations/locations.service.spec.ts
+++ src/locations/locations.service.spec.ts
@@ -1,18 +1,104 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LocationsService } from './locations.service';
+import { Location } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateLocationInput } from './dto/create-location.input';
+import { UpdateLocationInput } from './dto/update-location.input';
 
 describe('LocationsService', () => {
-  let service: LocationsService;
+  let locationsService: LocationsService;
+
+  const location: Location = {
+    id: 1100,
+    streetAddress: '93091 Calle della Testa',
+    postalCode: '10934',
+    city: 'Venice',
+    stateProvince: 'Venice',
+    countryId: 'IT',
+  };
+
+  const locations: Location[] = [
+    {
+      id: 1100,
+      streetAddress: '93091 Calle della Testa',
+      postalCode: '10934',
+      city: 'Venice',
+      stateProvince: 'Venice',
+      countryId: 'IT',
+    },
+  ];
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [LocationsService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService, LocationsService],
     }).compile();
 
-    service = module.get<LocationsService>(LocationsService);
+    locationsService = moduleRef.get<LocationsService>(LocationsService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create a location', async () => {
+      const createLocationInput: CreateLocationInput = {
+        streetAddress: '93091 Calle della Testa',
+        postalCode: '10934',
+        city: 'Venice',
+        stateProvince: 'Venice',
+        countryId: 'IT',
+      };
+      jest
+        .spyOn(locationsService, 'create')
+        .mockImplementation(async () => location);
+      expect(await locationsService.create(createLocationInput)).toBe(location);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of locations', async () => {
+      jest
+        .spyOn(locationsService, 'findAll')
+        .mockImplementation(async () => locations);
+      expect(await locationsService.findAll({ skip: 0, take: 10 })).toBe(
+        locations,
+      );
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a location by id', async () => {
+      jest
+        .spyOn(locationsService, 'findOne')
+        .mockImplementation(async () => location);
+      expect(await locationsService.findOne({ id: 1000 })).toBe(location);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a location', async () => {
+      const updateLocationInput: UpdateLocationInput = {
+        streetAddress: '93091 Calle della Testa',
+        postalCode: '10934',
+        city: 'Venice',
+        stateProvince: 'Venice',
+        countryId: 'IT',
+      };
+      jest
+        .spyOn(locationsService, 'update')
+        .mockImplementation(async () => location);
+      expect(
+        await locationsService.update({
+          where: { id: 1100 },
+          data: updateLocationInput,
+        }),
+      ).toBe(location);
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a location by id', async () => {
+      jest
+        .spyOn(locationsService, 'remove')
+        .mockImplementation(async () => location);
+      expect(await locationsService.remove({ id: 1000 })).toBe(location);
+    });
   });
 });
