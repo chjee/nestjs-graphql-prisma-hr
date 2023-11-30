104b4bcc1c166ea1bb7d280a68c10a2cd8c613bc
Changhoon-Jee <chjee71@gmail.com>
2023-11-30 14:53:15 +0900

    modify jest func. to countries resources.

--- src/countries/countries.resolver.spec.ts
+++ src/countries/countries.resolver.spec.ts
@@ -1,19 +1,116 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CountriesResolver } from './countries.resolver';
 import { CountriesService } from './countries.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegionsService } from '../regions/regions.service';
+import { Country } from './entities/country.entity';
+import { Region } from '../regions/entities/region.entity';
+import { CreateCountryInput } from './dto/create-country.input';
+import { UpdateCountryInput } from './dto/update-country.input';
 
 describe('CountriesResolver', () => {
-  let resolver: CountriesResolver;
+  let countriesResolver: CountriesResolver;
+  let countriesService: CountriesService;
+  let regionsService: RegionsService;
+
+  const country: Country = {
+    id: 'KR',
+    name: 'Republic of Korea',
+    regionId: 3,
+  };
+
+  const countries: Country[] = [
+    {
+      id: 'KR',
+      name: 'Republic of Korea',
+      regionId: 3,
+    },
+  ];
+
+  const region: Region = {
+    id: 5,
+    name: 'Antarctica',
+  };
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CountriesResolver, CountriesService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        PrismaService,
+        CountriesResolver,
+        CountriesService,
+        RegionsService,
+      ],
     }).compile();
 
-    resolver = module.get<CountriesResolver>(CountriesResolver);
+    countriesResolver = moduleRef.get<CountriesResolver>(CountriesResolver);
+    countriesService = moduleRef.get<CountriesService>(CountriesService);
+    regionsService = moduleRef.get<RegionsService>(RegionsService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  describe('create', () => {
+    it('should create a country', async () => {
+      const createCountryInput: CreateCountryInput = {
+        id: 'KR',
+        name: 'Republic of Korea',
+        regionId: 3,
+      };
+      jest
+        .spyOn(countriesService, 'create')
+        .mockImplementation(async () => country);
+      expect(await countriesResolver.createCountry(createCountryInput)).toBe(
+        country,
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of countries', async () => {
+      jest
+        .spyOn(countriesService, 'findAll')
+        .mockImplementation(async () => countries);
+      expect(await countriesResolver.findAll(0, 10)).toBe(countries);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a country by id', async () => {
+      jest
+        .spyOn(countriesService, 'findOne')
+        .mockImplementation(async () => country);
+      expect(await countriesResolver.findOne('KR')).toBe(country);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a country by id', async () => {
+      const updateCountryInput: UpdateCountryInput = {
+        name: 'Republic of Korea',
+        regionId: 3,
+      };
+      jest
+        .spyOn(countriesService, 'update')
+        .mockImplementation(async () => country);
+      expect(
+        await countriesResolver.updateCountry('KR', updateCountryInput),
+      ).toBe(country);
+    });
+  });
+
+  describe('region', () => {
+    it('should return a region by id', async () => {
+      jest
+        .spyOn(regionsService, 'findOne')
+        .mockImplementation(async () => region);
+      expect(await countriesResolver.region(country)).toBe(region);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a country by id', async () => {
+      jest
+        .spyOn(countriesService, 'remove')
+        .mockImplementation(async () => country);
+      expect(await countriesResolver.removeCountry('KR')).toBe(country);
+    });
   });
 });
--- src/countries/countries.service.spec.ts
+++ src/countries/countries.service.spec.ts
@@ -1,18 +1,93 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CountriesService } from './countries.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { Country } from '@prisma/client';
+import { CreateCountryInput } from './dto/create-country.input';
+import { UpdateCountryInput } from './dto/update-country.input';
 
 describe('CountriesService', () => {
-  let service: CountriesService;
+  let countriesService: CountriesService;
+
+  const country: Country = {
+    id: 'KR',
+    name: 'Republic of Korea',
+    regionId: 3,
+  };
+
+  const countries: Country[] = [
+    {
+      id: 'KR',
+      name: 'Republic of Korea',
+      regionId: 3,
+    },
+  ];
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CountriesService],
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [PrismaService, CountriesService],
     }).compile();
 
-    service = module.get<CountriesService>(CountriesService);
+    countriesService = moduleRef.get<CountriesService>(CountriesService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('create', () => {
+    it('should create a country', async () => {
+      const createCountryInput: CreateCountryInput = {
+        id: 'KR',
+        name: 'Korea',
+        regionId: 3,
+      };
+      jest
+        .spyOn(countriesService, 'create')
+        .mockImplementation(async () => country);
+      expect(await countriesService.create(createCountryInput)).toBe(country);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of countries', async () => {
+      jest
+        .spyOn(countriesService, 'findAll')
+        .mockImplementation(async () => countries);
+      expect(await countriesService.findAll({ skip: 0, take: 3 })).toBe(
+        countries,
+      );
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a country by id', async () => {
+      jest
+        .spyOn(countriesService, 'findOne')
+        .mockImplementation(async () => country);
+      expect(await countriesService.findOne({ id: 'KR' })).toBe(country);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a country by id', async () => {
+      const updateCountryInput: UpdateCountryInput = {
+        name: 'Republic of Korea',
+        regionId: 3,
+      };
+      jest
+        .spyOn(countriesService, 'update')
+        .mockImplementation(async () => country);
+      expect(
+        await countriesService.update({
+          where: { id: 'KR' },
+          data: updateCountryInput,
+        }),
+      ).toBe(country);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a country by id', async () => {
+      jest
+        .spyOn(countriesService, 'remove')
+        .mockImplementation(async () => country);
+      expect(await countriesService.remove({ id: 'KR' })).toBe(country);
+    });
   });
 });
