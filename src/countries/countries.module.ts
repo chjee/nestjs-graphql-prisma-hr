1afe678eda1d942c9a9bea9d0e9e8453eb2cf236
Changhoon-Jee <chjee71@gmail.com>
2023-12-02 14:47:42 +0900

    remove location service module.

--- src/countries/countries.module.ts
+++ src/countries/countries.module.ts
@@ -2,14 +2,8 @@ import { Module } from '@nestjs/common';
 import { CountriesService } from './countries.service';
 import { CountriesResolver } from './countries.resolver';
 import { RegionsService } from '../regions/regions.service';
-import { LocationsService } from '../locations/locations.service';
 
 @Module({
-  providers: [
-    CountriesResolver,
-    CountriesService,
-    RegionsService,
-    LocationsService,
-  ],
+  providers: [CountriesResolver, CountriesService, RegionsService],
 })
 export class CountriesModule {}
