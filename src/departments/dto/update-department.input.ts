0a8881e09794e481e7acda9a571f567d02f0d5f9
Changhoon-Jee <chjee71@gmail.com>
2023-12-03 19:05:50 +0900

    apply partial type.

--- src/departments/dto/update-department.input.ts
+++ src/departments/dto/update-department.input.ts
@@ -1,7 +1,8 @@
 import { CreateDepartmentInput } from './create-department.input';
-import { InputType, OmitType } from '@nestjs/graphql';
+import { InputType, OmitType, PartialType } from '@nestjs/graphql';
 
 @InputType({ description: 'Update department input' })
-export class UpdateDepartmentInput extends OmitType(CreateDepartmentInput, [
-  'id',
-] as const) {}
+export class UpdateDepartmentInput extends OmitType(
+  PartialType(CreateDepartmentInput),
+  ['id'] as const,
+) {}
