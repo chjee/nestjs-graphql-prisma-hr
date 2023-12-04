13b7b8de0a3ecb660f916876798dc45da6939c06
Changhoon-Jee <chjee71@gmail.com>
2023-12-04 18:01:13 +0900

    modify import path.

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { JobsResolver } from './jobs.resolver';
 import { EmployeesService } from '../employees/employees.service';
-import { JobhistoriesService } from 'src/jobhistories/jobhistories.service';
+import { JobhistoriesService } from '../jobhistories/jobhistories.service';
 
 @Module({
   providers: [JobsResolver, JobsService, EmployeesService, JobhistoriesService],
