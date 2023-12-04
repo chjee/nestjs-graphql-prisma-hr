5920ae6fcfd556f3bbe6e22c69629cbc99d60813
Changhoon-Jee <chjee71@gmail.com>
2023-12-04 16:09:46 +0900

    add partial type.

--- src/jobhistories/dto/update-jobhistory.input.ts
+++ src/jobhistories/dto/update-jobhistory.input.ts
@@ -1,7 +1,8 @@
 import { CreateJobhistoryInput } from './create-jobhistory.input';
-import { InputType, OmitType } from '@nestjs/graphql';
+import { InputType, OmitType, PartialType } from '@nestjs/graphql';
 
 @InputType({ description: 'Update jobhistory input' })
-export class UpdateJobhistoryInput extends OmitType(CreateJobhistoryInput, [
-  'employeeId',
-] as const) {}
+export class UpdateJobhistoryInput extends OmitType(
+  PartialType(CreateJobhistoryInput),
+  ['employeeId', 'startedAt'] as const,
+) {}
